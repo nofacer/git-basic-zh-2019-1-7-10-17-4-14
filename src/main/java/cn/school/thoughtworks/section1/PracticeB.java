f9a56fecfcd93158c755eae01a80908d398d6524
nofacer <nofacer@outlook.com>
2019-03-16 21:45:32 +0800

    1B

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,9 +1,18 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-
+        List<String> result = new ArrayList<>();
+        for (String str : collection1) {
+            for (List<String> subCollection : collection2) {
+                if (subCollection.contains(str)) {
+                    result.add(str);
+                }
+            }
+        }
+        return result;
     }
 }
