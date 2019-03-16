88a000753c58c73be2aae143def6c71f724aa11a
nofacer <nofacer@outlook.com>
2019-03-16 21:32:07 +0800

    1A

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+
     }
 }
